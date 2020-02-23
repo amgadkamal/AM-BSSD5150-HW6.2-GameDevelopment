92eb3da7538964c68cc5b8e8df5d830f1974f32b
Amgad Morsy <42896629+amgadkamal@users.noreply.github.com>
2020-02-22 22:57:15 -0700

    HW6.2

---
+++ AM-SSD5150-HW6.2- GameDev/Assets/Scripts/GameControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //press w for walking, release W for idle
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger ("State", 1);
+        }
+        if (Input.GetKeyUp (KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+        //press R for runing, release w for idle
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetInteger("State", 2);
+         }
+        if (Input.GetKeyUp(KeyCode.R))
+            {
+                anim.SetInteger("State", 0);
+            }
+
+    }
+    
+}
